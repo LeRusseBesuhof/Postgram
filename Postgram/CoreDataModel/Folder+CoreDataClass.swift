959b22d8d2df8460f22df4840d5edaf478f6c62d
LeRusseBesuhof <p.grad0ff@gmail.com>
2024-07-09 00:32:45 +0700

    v 0.1 (initial commit)

---
+++ Postgram/CoreDataModel/Folder+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Folder+CoreDataClass.swift
+//  Postgram
+//
+//  Created by Павел Градов on 09.07.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Folder)
+public class Folder: NSManagedObject {
+
+}
---
+++ Postgram/CoreDataModel/Post+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Post+CoreDataClass.swift
+//  Postgram
+//
+//  Created by Павел Градов on 09.07.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Post)
+public class Post: NSManagedObject {
+
+}
